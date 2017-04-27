81a6100abdd61957e92bbb0801b39ec132922440
Simone Nathaniel <shedamoe@gmail.com>
2017-04-27 14:07:26 -0600

    moved foleder

--- user-admin-sync/config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by sim_one_n_only on 4/26/17.
- */
-module.exports = {
-    secret: "home sick like a mugg",
-    database: "mongodb://localhost/schedule"
-}
--- user-admin-sync/public/components/calendar/calendar.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by sim_one_n_only on 4/26/17.
- */
-var app = angular.module("bookingApp");
-
-app.controller("calendarCntrl", ["$scope", function ($scope) {
-    $scope.testgir
-}])
